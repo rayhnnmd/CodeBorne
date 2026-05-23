8eb7114f69acc312400f6cb192325a96ba440053
Rayhnn <rayhnmd024@gmail.com>
2026-05-23 19:17:32 +0530

    added services stubs

---
+++ utils/helpers.py
@@ -0,0 +1,18 @@
+def seconds_to_hours(seconds, decimals=1):
+    return round(seconds / 3600, decimals)
+
+def seconds_to_display(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    if hours > 0:
+        return f"{minutes}m"
+        
+def get_rarity_color(rarity):
+    colors = {
+        "common":    "#aaaaaa",
+        "rare":      "#4fc3f7",
+        "epic":      "#ce93d8",
+        "legedary":  "#f0c040",
+        "mythic":    "#ff6b6b",
+    }
+    return colors.get(rarity, "#aaaaaa")
